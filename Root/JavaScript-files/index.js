0194e5d1347cfbe9f3f2dec53642be31a175f892
Abdulqadir Ajabsha <abdulqadirajabsha@gmail.com>
2023-03-04 11:42:03 +0530

    first commit

---
+++ Root/JavaScript-files/index.js
@@ -0,0 +1,9 @@
+
+let count=0;
+increment();
+function increment()
+{   
+    document.getElementById("number").textContent= count++;
+    
+}
+
---
+++ Root/JavaScript-files/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+function func(value1,value2)
+// {   
+//     return myobj[value1].list[value1];
+// }
